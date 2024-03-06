f687856e3f933ab4eb898e4d7faba7ed6a5ecc1f
SYED FAIZ AHMED <sfahmed129@gmail.com>
2024-03-06 12:31:51 +0500

    JSON & It's common Operations

--- app.js
+++ app.js
@@ -48,3 +48,12 @@
 //     this.name = 'Faiz';
 // }
 // console.log(obj)
+
+// JSON & its common operations
+//Definition
+// Parsing: Converting a string to a native object
+// JSON.parse(text)
+// Stringification: converting a native object to a string so it can be transmitted across the network
+// JSON.stringify(object)
+
+
